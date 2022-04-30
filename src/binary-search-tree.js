e6047660f5a254e834f4b3a38bfc56e5f6c5482d
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-04-30 03:40:41 +0300

    feat: slove binary tree find()

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -18,17 +18,44 @@ class BinarySearchTree {
     let node = new Node(data)
     if(this.head === null){
       this.head = node
+    } else {
+      this.addNode(this.head, node);
     }
   }
+  addNode(node, newNode){
+    if(newNode.data < node.data){
+       if(node.left === null){
+          node.left = newNode;
+       }else{
+          this.addNode(node.left, newNode);
+       };
+    } else {
+       if(node.right === null){
+          node.right = newNode;
+       }else{
+          this.addNode(node.right,newNode);
+       };
+    };
+ };
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return this.search(this.head, data);
+  }
+   search(node, data){
+    if(node === null){
+      return null
+    }else if(node.data > data){
+      return this.search(node.left, data);
+    }else if(node.data < data){
+      return this.search(node.right, data);
+    }else{
+      return node;
+    }
   }
 
   remove(/* data */) {
